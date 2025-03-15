e6dcba6c6c5f436f1ee17920afc55de897da3787
Tiago Gonçalves <tiagogn@gmail.com>
2025-03-15 19:56:33 -0300

    fix: desativando o springboottest por enquanto

--- src/test/java/br/com/fiap/hackaton/processor/ProcessorApplicationTests.java
+++ src/test/java/br/com/fiap/hackaton/processor/ProcessorApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.fiap.hackaton.processor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProcessorApplicationTests {
 
 	@Test
