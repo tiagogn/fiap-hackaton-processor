41ff7769b3b130b6402134b00c03737821b8507c
roseanecosta <roseanecosta88@gmail.com>
2025-04-17 18:51:10 -0300

    ajustando para funcionamento na AWS

--- src/main/java/br/com/fiap/hackaton/processor/infrastructure/notifier/SendoNotificationErrorByEmail.java
+++ src/main/java/br/com/fiap/hackaton/processor/infrastructure/notifier/SendoNotificationErrorByEmail.java
@@ -3,6 +3,7 @@ package br.com.fiap.hackaton.processor.infrastructure.notifier;
 import br.com.fiap.hackaton.processor.core.domain.Notification;
 import br.com.fiap.hackaton.processor.core.notifier.SendNotificationError;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,9 @@ public class SendoNotificationErrorByEmail implements SendNotificationError {
 
     private final MailSender mailSender;
 
+    @Value("${aws.ses.email-from}")
+    private String defaultFrom;
+
     public SendoNotificationErrorByEmail(MailSender mailSender) {
         this.mailSender = mailSender;
     }
@@ -20,7 +24,7 @@ public class SendoNotificationErrorByEmail implements SendNotificationError {
     @Override
     public void sendNotification(Notification notification) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("fiap-processor@gmail.com");
+        message.setFrom(defaultFrom);
         message.setTo(notification.getTo());
         message.setSubject(notification.getSubject());
         message.setText(notification.getBody());
